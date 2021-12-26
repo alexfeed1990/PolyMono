97163392cb68d5aac641f522508f465e8ff291f2
alexfeed1990 <alexfeed1990real@gmail.com>
2021-12-26 22:54:58 +0200

    test button

--- Client/main.js
+++ Client/main.js
@@ -2,13 +2,12 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
     //Modules
-
     const { screen } = require('electron') //Cant load until app is ready
 
     const primaryDisplay = screen.getPrimaryDisplay()
     const { width, height } = primaryDisplay.workAreaSize //Get h, w of display
 
-    const win = new BrowserWindow({width, height})
+    const win = new BrowserWindow({width, height, autoHideMenuBar: true})
     win.loadFile('index.html') //Load index.html
 }
 
