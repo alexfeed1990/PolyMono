2b8f1ed2e100255f9c8168326d3d07b773a2affa
alexfeed1990 <alexfeed1990real@gmail.com>
2021-12-26 21:31:48 +0200

    Add electron base files

---
+++ Client/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+    //Modules
+
+    const { screen } = require('electron') //Cant load until app is ready
+
+    const primaryDisplay = screen.getPrimaryDisplay()
+    const { width, height } = primaryDisplay.workAreaSize //Get h, w of display
+
+    const win = new BrowserWindow({width, height})
+    win.loadFile('index.html') //Load index.html
+}
+
+function reqModules() {
+    const { screen } = require('electron')
+
+}
+
+app.whenReady().then(() => {
+    reqModules()
+    createWindow()
+})
